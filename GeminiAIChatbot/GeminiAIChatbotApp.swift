b45d8151afc191e4c10f340a1e113ab62e8fbeee
Aruna Udayanga <arunaudayanga@Arunas-MacBook-Pro.local>
2024-06-12 01:28:57 +0100

    Initial commit

---
+++ GeminiAIChatbot/GeminiAIChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAIChatbotApp.swift
+//  GeminiAIChatbot
+//
+//  Created by Aruna Udayanga on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeminiAIChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
